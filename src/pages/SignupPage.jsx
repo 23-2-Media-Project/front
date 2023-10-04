dbd2520fe254179faee94c59fa1b0ade6bcac007
hyeseung <wjoo3@naver.com>
2023-10-04 22:11:45 +0900

    docs: Signup.scss 파일 이름 변경 (SignupPage.scss)

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Signup.scss";
+import "../styles/SignupPage.scss";
 
 import { ReactComponent as KakaoIcon } from "../assets/icons/kakao-logo.svg";
 import { ReactComponent as NaverIcon } from "../assets/icons/naver-logo.svg";
@@ -7,7 +7,7 @@ import { ReactComponent as NaverIcon } from "../assets/icons/naver-logo.svg";
 import BottomLicenseText from "../components/signup/BottomLicenseText";
 
 /* TODO: 로그인/회원가입 페이지
- * [ ] 소셜로그인 페이지 퍼블리싱
+ * [O] 소셜로그인 페이지 퍼블리싱
  * [ ] 소셜로그인 기능 구현 (인증코드와 Redirect URI 전송해 사이트 토큰 발급받기)
  * [ ] 관심 주제 선택 페이지 퍼블리싱
  * [ ] 관심 주제 선택 기능 구현 (선택한 관심 주제 백엔드로 전달)
