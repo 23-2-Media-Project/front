ff73da6b8dd7cb97cf1e13ff2a69da790e614df6
hyeseung <wjoo3@naver.com>
2023-10-04 22:08:55 +0900

    docs: BottomText.jsx 파일 이름 변경 (BottomLicenseText.jsx)

--- src/components/signup/BottomLicenseText.jsx
+++ src/components/signup/BottomLicenseText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 // 라이선스 관련 하단 텍스트
-const BottomText = () => {
+const BottomLicenseText = () => {
   return <Text>©Artwalk. All Rights Reserved.</Text>;
 };
 
@@ -10,4 +10,4 @@ const Text = styled.div`
   margin: 20px auto 0;
   font-size: 16px;
 `;
-export default BottomText;
+export default BottomLicenseText;
