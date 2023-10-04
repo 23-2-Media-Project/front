f3b01fda7d6dd63045ab9685c147a9f3e169080d
hyeseung <wjoo3@naver.com>
2023-10-04 22:31:36 +0900

    feature: 관심 주제 선택 섹션 회원가입 페이지에 추가

--- src/components/signup/StartingSection.jsx
+++ src/components/signup/StartingSection.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import { ReactComponent as KakaoIcon } from "../../assets/icons/kakao-logo.svg";
 import { ReactComponent as NaverIcon } from "../../assets/icons/naver-logo.svg";
 
-const StartingSection = () => {
+const StartingSection = ({ setClickBtn }) => {
   return (
     <div className="content-wrap">
       <span className="title">로그인/회원가입</span>
       <span className="sub-title">소셜로그인을 통해 로그인/회원가입 진행</span>
       <div className="signup-btn-wrap">
-        <button className="signup-btn">
+        <button className="signup-btn" onClick={() => setClickBtn(true)}>
           <KakaoIcon width={24} height={24} />
           <span className="btn-text">카카오로 시작하기</span>
         </button>
-        <button className="signup-btn">
+        <button className="signup-btn" onClick={() => setClickBtn(true)}>
           <NaverIcon width={24} height={24} />
           <span className="btn-text">네이버로 시작하기</span>
         </button>
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/SignupPage.scss";
 
 import BottomLicenseText from "../components/signup/BottomLicenseText";
 import StartingSection from "../components/signup/StartingSection";
+import InterestsSection from "../components/signup/InterestsSection";
 
 /* TODO: 로그인/회원가입 페이지
  * [O] 소셜로그인 페이지 퍼블리싱
@@ -13,9 +14,21 @@ import StartingSection from "../components/signup/StartingSection";
 
 const SignupPage = () => {
   //FIXME: 카카오 네이버 버튼 눌림 여부 state (추후 첫 유저인지 기존 유저인지 구분에 따라 관심사 설정 페이지가 보일지 말지 결정)
+  const [isClickBtn, setClickBtn] = useState(false);
+
+  //FIXME: 수정 후 삭제 필요
+  useEffect(() => {
+    setClickBtn(false);
+  }, []);
+
+  console.log(isClickBtn);
   return (
     <div className="signup-container">
-      <StartingSection />
+      {isClickBtn ? (
+        <InterestsSection />
+      ) : (
+        <StartingSection setClickBtn={setClickBtn} />
+      )}
       <BottomLicenseText />
     </div>
   );
