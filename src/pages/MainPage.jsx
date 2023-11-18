64fa8827f3e28ddd02fee0720f2db7a769ba9859
hyeseung <wjoo3@naver.com>
2023-11-19 03:27:30 +0900

    feature: 이달의 전시회 이동 버튼 페이지 이동 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import { ReactComponent as Logo3d } from "../assets/logo/logo-3d.svg";
 
 import "../styles/MainPage.scss";
+
 /* TODO:
  * [O] 3D 로고 애니메이션 적용
- * [] 이달의 전시회 버튼 컴포넌트 구현
+ * [O] 이달의 전시회 버튼 컴포넌트 구현
  * [] 추천/인기/최신 전시회 목록 컴포넌트 구현
  */
 const MainPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="main-container">
       <div className="left-wrap">
@@ -17,7 +21,12 @@ const MainPage = () => {
         </div>
         <div className="monthly-exhibition-btn-wrap">
           <div className="btn-title">12월의 전시회 입장하기</div>
-          <button className="monthly-exhibition-btn">
+          <button
+            className="monthly-exhibition-btn"
+            onClick={() => {
+              navigate("/mothly");
+            }}
+          >
             <div className="btn-text white">CHRISTMAS</div>
             <div className="btn-text green">CHRISTMAS</div>
             <div className="btn-text red">CHRISTMAS</div>
