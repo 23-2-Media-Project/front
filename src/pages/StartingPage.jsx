79f4a1e82067521b60f9570c1c54962317e3929c
hyeseung <wjoo3@naver.com>
2023-11-19 00:42:57 +0900

    feature: 마우스 따라다니는 원형 UI 구현

--- src/pages/StartingPage.jsx
+++ src/pages/StartingPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import "../styles/StartingPage.scss";
 
 const StartingPage = () => {
-  return <div>StartingPage</div>;
+  // 마우스 x,y 좌표를 담는 state
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+  // 마우스 움직일 때 x,y 좌표 변경하는 함수
+  const setMousePosition = useCallback((e) => {
+    setMouseX(e.clientX);
+    setMouseY(e.clientY);
+  }, []);
+
+  return (
+    <div className="starting-container" onMouseMove={setMousePosition}>
+      <div
+        className="circle"
+        style={{
+          top: mouseY,
+          left: mouseX,
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default StartingPage;
