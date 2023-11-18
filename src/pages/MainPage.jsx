0d71f1e1e9335298a93b00a630692b419a6447a9
hyeseung <wjoo3@naver.com>
2023-11-19 02:50:00 +0900

    feature: 3d 로고 위아래 움직임 거리 및 시간 조절

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Logo3d } from "../assets/logo/logo-3d.svg";
 
 import "../styles/MainPage.scss";
 /* TODO:
- * [] 3D 로고 애니메이션 적용
+ * [O] 3D 로고 애니메이션 적용
  * [] 이달의 전시회 버튼 컴포넌트 구현
  * [] 추천/인기/최신 전시회 목록 컴포넌트 구현
  */
