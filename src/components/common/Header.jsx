ba232559c8882e1541820823ed972126a9035c64
hyeseung <wjoo3@naver.com>
2023-11-18 22:33:03 +0900

    feature: 헤더 스타일 및 기능 변경

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,57 +1,91 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
+
+import { ReactComponent as Logo } from "../../assets/logo/logo.svg";
 
 /* FIXME: 헤더 컴포넌트 (임시로 디자인 완료 후 수정 필요)
  * TODO:
- * [ ] 로고 삽입
+ * [O] 로고 삽입
  * [ ] 홈 화면 이동 버튼 생성
  * [ ] 전시 페이지 이동 버튼 생성
- * [ ] 파트너 페이지 이동 버튼 생성
  * [ ] 마이페이지 이동 버튼 생성
- * [ ] 우측 로그인 전 : 로그인/회원가입 버튼 생성
- * [ ] 우측 로그인 후 : 프로필 사진 및 드롭다운
+ * [ ] 로그인/로그라웃 버튼 생성
  *
  */
+
 const Header = () => {
+  // 현재 경로 확인 변수
+  let location = useLocation();
+
   return (
     <HeaderWrap>
+      <Link to="/">
+        <Logo style={{ width: "50px" }} />
+      </Link>
       <PageBtnWrap>
-        <PageBtn to="/">홈</PageBtn>
-        <PageBtn>전시</PageBtn>
-        <PageBtn>마이페이지</PageBtn>
+        {location?.pathname === "/" ? (
+          <PageBtn to="/" style={{ marginLeft: "5vw" }}>
+            Home
+          </PageBtn>
+        ) : (
+          <PageBlurBtn to="/" style={{ marginLeft: "5vw" }}>
+            Home
+          </PageBlurBtn>
+        )}
+        {location?.pathname === "/exhibition" ? (
+          <PageBtn to="exhibition">Exhibition</PageBtn>
+        ) : (
+          <PageBlurBtn to="exhibition">Exhibition</PageBlurBtn>
+        )}
+        {location?.pathname === "/mypage" ? (
+          <PageBtn to="mypage">MyPage</PageBtn>
+        ) : (
+          <PageBlurBtn to="mypage">MyPage</PageBlurBtn>
+        )}
       </PageBtnWrap>
-      <SignupBtn to="signup">로그인/회원가입</SignupBtn>
+      <SignupBtn to="signup">LogIn</SignupBtn>
     </HeaderWrap>
   );
 };
 
 const SignupBtn = styled(Link)`
   position: absolute;
-  right: 2%;
+  right: 30px;
   width: max-content;
   text-decoration: none;
   cursor: pointer;
-  color: #000;
+  color: #a88bf7;
 `;
 const PageBtn = styled(Link)`
   text-decoration: none;
   cursor: pointer;
-  color: #000;
+  color: #a88bf7;
+`;
+const PageBlurBtn = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+  color: #a88bf7;
+  filter: blur(1.2px);
+  transition: 0.5s;
+  &:hover {
+    filter: blur(0);
+  }
 `;
 const PageBtnWrap = styled.div`
   display: flex;
-  gap: 8vw;
+  gap: 5vw;
 `;
 const HeaderWrap = styled.div`
   position: relative;
   display: flex;
   align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 80px;
-  background-color: red;
-  padding: 0 2%;
+  width: max;
+  height: 60px;
+  margin: 30px 30px 0 30px;
+  background-color: #000;
+  border-radius: 100px;
+  padding-left: 20px;
   box-sizing: border-box;
 `;
 
