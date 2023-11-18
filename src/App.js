e75bf3c14b25453817df9a2c0043eb25db8c616f
hyeseung <wjoo3@naver.com>
2023-11-18 22:28:34 +0900

    feature: 전시 페이지, 마이 페이지 react router 적용

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
 import SignupPage from "./pages/SignupPage";
+import ExhibitionPage from "./pages/ExhibitionPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/signup" element={<SignupPage />}></Route>
+      <Route path="/exhibition" element={<ExhibitionPage />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 }
