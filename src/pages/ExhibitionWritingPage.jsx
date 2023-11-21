09ac74d215c464cd62a10a7d38dcf93e6649d870
hyeseung <wjoo3@naver.com>
2023-11-21 17:48:59 +0900

    docs: 전시회 작성 페이지 파일 생성

---
+++ src/pages/ExhibitionWritingPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../styles/ExhibitionWritingPage.scss";
+
+const ExhibitionWritingPage = () => {
+  return <div>전시회 작성</div>;
+};
+
+export default ExhibitionWritingPage;
