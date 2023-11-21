fe88f05854837b3b7b230e5f4ee2fb97c57d2517
hyeseung <wjoo3@naver.com>
2023-11-21 22:36:13 +0900

    feature: linear-gradient 및 스크롤시 커지는 원형 배경 적용

--- src/pages/MonthlyExhibitionPage.jsx
+++ src/pages/MonthlyExhibitionPage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { motion, useTransform, useScroll, useMotionValue } from "framer-motion";
 
 import "../styles/MonthlyExhibitionPage.scss";
 
 const MonthlyExhibitionPage = () => {
-  return <div className="monthly-container">MonthlyExhibitionPage</div>;
+  const x = useMotionValue(0);
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
+  return (
+    <div className="monthly-container">
+      <motion.div
+        className="circle-background"
+        style={{ x, scale }}
+        drag="x"
+        dragSnapToOrigin
+      ></motion.div>
+    </div>
+  );
 };
 
 export default MonthlyExhibitionPage;
