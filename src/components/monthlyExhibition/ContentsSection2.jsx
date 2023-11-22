038584b95320b5177d5e6982c8ef275b0e9ad139
hyeseung <wjoo3@naver.com>
2023-11-23 00:36:26 +0900

    feature: 이달의 전시회 목록 구현

--- src/components/monthlyExhibition/ContentsSection2.jsx
+++ src/components/monthlyExhibition/ContentsSection2.jsx
@@ -2,8 +2,26 @@ import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ReactComponent as WhiteArrowIcon } from "../../assets/icons/white-arrow.svg";
+import { ReactComponent as FavoriteIcon } from "../../assets/icons/favorite.svg";
+import { ReactComponent as ShareIcon } from "../../assets/icons/share.svg";
 
 import CategorySelectMenu from "./CategorySelectMenu";
+
+const HoverContents = () => {
+  return (
+    <div className="hover-contents">
+      <div className="title-wrap">
+        <div className="title">전시회 제목</div>
+        <div className="topic">전시분야</div>
+      </div>
+      <div className="icon-wrap">
+        <FavoriteIcon style={{ width: "0.8vw" }} />
+        <div className="like-count">99</div>
+        <ShareIcon style={{ width: "0.8vw" }} />
+      </div>
+    </div>
+  );
+};
 
 const ContentsSection2 = () => {
   const navigate = useNavigate();
@@ -27,6 +45,10 @@ const ContentsSection2 = () => {
   // 확장 버튼 클릭 여부 state
   const [onClickBtn, setClickBtn] = useState(false);
 
+  const [hoverIdx, setHoverIdx] = useState(-1);
+
+  const boxArr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
   return (
     <div className="section2-container">
       <WhiteArrowIcon
@@ -42,6 +64,25 @@ const ContentsSection2 = () => {
           setClickBtn={setClickBtn}
           handleClick={handleClick}
         />
+        <div className="middle-line"></div>
+        <div className="content-box-wrap">
+          {boxArr.map((box, idx) => (
+            <div
+              key={idx}
+              className="content-box"
+              onMouseOver={() => {
+                setHoverIdx(idx);
+              }}
+              onMouseLeave={() => {
+                setHoverIdx(-1);
+              }}
+            >
+              <div className={`hover-wrap ${hoverIdx === idx && "active"}`}>
+                <HoverContents />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
