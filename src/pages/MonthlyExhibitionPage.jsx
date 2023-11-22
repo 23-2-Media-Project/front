4baa0c0eae20b311f95035836812215e931e53fc
hyeseung <wjoo3@naver.com>
2023-11-22 14:47:41 +0900

    feature: 이달의 전시회 첫 페이지 상세 내용 스크롤시 생성

--- src/pages/MonthlyExhibitionPage.jsx
+++ src/pages/MonthlyExhibitionPage.jsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, useTransform, useScroll, useMotionValue } from "framer-motion";
 
 import "../styles/MonthlyExhibitionPage.scss";
 
 const MonthlyExhibitionPage = () => {
+  // 스크롤시 크기 변형되는 애니메이션 관련
   const x = useMotionValue(0);
   const { scrollYProgress } = useScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
+  // 스크롤시 이달의 전시회 제목과 추천 전시회 등 상세 요소들 보이게 하는 코드
+  const scrollRef = useRef();
+  const [position, setPosition] = useState(0); // 현재 스크롤 위치를 담는 state
+  const [isShow, setIsShow] = useState(false); // 스크롤시 true가 되며 요소를 보이게 하는 state
+  // 스크롤 핸들러 함수
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+  // 스크롤 이벤트 리스너
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+  // 현재 스크롤 위치가 0보다 크면 isShow state true 아니면 false
+  useEffect(() => {
+    if (position > 0) {
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+    }
+  }, [position]);
+
   return (
-    <div className="monthly-container">
+    <div className="monthly-container" ref={scrollRef}>
       <motion.div
         className="circle-background"
         style={{ x, scale }}
         drag="x"
         dragSnapToOrigin
       ></motion.div>
+      <div
+        className="circle-inner-wrap"
+        style={{ opacity: `${isShow ? "1" : "0"}` }}
+      >
+        <button>하이</button>
+      </div>
     </div>
   );
 };
