0778eaf8afa12193fd8aed405e37bba6692f4201
hyeseung <wjoo3@naver.com>
2023-11-22 17:05:36 +0900

    feature: 이달의 전시회 목록 섹션 파일 추가

---
+++ src/components/monthlyExhibition/ContentsSection2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { ReactComponent as WhiteArrowIcon } from "../../assets/icons/white-arrow.svg";
+
+const ContentsSection2 = () => {
+  return <div className="section2-container">ContentsSection2</div>;
+};
+
+export default ContentsSection2;
