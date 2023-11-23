fc128a761d761bcf8988de9e2aa6d9a29939e648
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-23 17:41:34 +0900

    feature: DefaultInfo 컴포넌트 연결

---
+++ src/components/exhibition-writing/DefaultInfo.jsx
@@ -0,0 +1,11 @@
+const DefaultInfo = () => {
+  return (
+    <>
+      <section>
+        <div>기본 정보 입력</div>
+      </section>
+    </>
+  );
+};
+
+export default DefaultInfo;
--- src/pages/ExhibitionWritingPage.jsx
+++ src/pages/ExhibitionWritingPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "../styles/ExhibitionWritingPage.scss";
+import DefaultInfo from "../components/exhibition-writing/DefaultInfo";
 
 const ExhibitionWritingPage = () => {
-  return <div className="writing-container">전시회 작성</div>;
+  return (
+    <div className="writing-container">
+      <DefaultInfo />
+    </div>
+  );
 };
 
 export default ExhibitionWritingPage;
