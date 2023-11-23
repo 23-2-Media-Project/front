74122b635e0ef56dc02d0d02ce0fc10e0311dee5
hyeseung <wjoo3@naver.com>
2023-11-23 23:48:58 +0900

    feature: 최신순/인기순 필터링 구현

--- src/pages/ExhibitionPage.jsx
+++ src/pages/ExhibitionPage.jsx
@@ -2,13 +2,15 @@ import React, { useCallback, useState } from "react";
 
 import "../styles/ExhibitionPage.scss";
 
+import { ReactComponent as FilterIcon } from "../assets/icons/filter.svg";
+
 import ListHoverContents from "../components/common/ListHoverContents";
 import TopicCategorySelectMenu from "../components/exhibition/TopicCategorySelectMenu";
 
 /* TODO:
- * [] 최신순, 인기순, 필터링 버튼 및 툴팁 구현
- * [] 주제 및 분야 선택 아코디언 메뉴 구현
- * [] 전시회 목록 구현
+ * [O] 최신순, 인기순, 필터링 버튼 및 툴팁 구현
+ * [O] 주제 및 분야 선택 아코디언 메뉴 구현
+ * [O] 전시회 목록 구현
  */
 
 const ExhibitionPage = () => {
@@ -33,12 +35,48 @@ const ExhibitionPage = () => {
   // 확장 버튼 클릭 여부 state
   const [onClickBtn, setClickBtn] = useState(false);
 
+  // hover된 목록의 index 담는 state
   const [hoverIdx, setHoverIdx] = useState(-1);
+
+  const [onClickFilterBtn, setClickFilterBtn] = useState(false);
+  const [filterOption, setFilterOption] = useState("최신순");
 
   return (
     <div className="exhibition-container">
       <div className="list-wrap">
-        <div className="title">전시회 목록</div>
+        <div className="title-wrap">
+          <div className="title">전시회 목록</div>
+          <div
+            className="filter-btn"
+            onClick={() => setClickFilterBtn(!onClickFilterBtn)}
+          >
+            <div className="filter-btn-contents">
+              <FilterIcon style={{ width: "1.2vw" }} />
+              {filterOption}
+            </div>
+            {onClickFilterBtn && (
+              <div className="filter-dropdown">
+                <div
+                  className={`option-text ${
+                    filterOption === "최신순" && "active"
+                  }`}
+                  onClick={() => setFilterOption("최신순")}
+                >
+                  최신순
+                </div>
+                <div
+                  className={`option-text ${
+                    filterOption === "인기순" && "active"
+                  }`}
+                  onClick={() => setFilterOption("인기순")}
+                >
+                  인기순
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+
         <TopicCategorySelectMenu
           onClickBtn={onClickBtn}
           setClickBtn={setClickBtn}
