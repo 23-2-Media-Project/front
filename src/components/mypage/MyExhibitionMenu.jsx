8551f426e78aef0f99da0ad7f58b32abb27f8b0a
hyeseung <wjoo3@naver.com>
2023-11-26 02:16:30 +0900

    feature: 마이페이지 - 나의 전시회 메뉴 구현

--- src/components/mypage/MyExhibitionMenu.jsx
+++ src/components/mypage/MyExhibitionMenu.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ListHoverContent from "../common/ListHoverContents";
 
 const MyExhibitionMenu = () => {
-  return <div>MyExhibitionMenu</div>;
+  const [hoverIdx, setHoverIdx] = useState(-1);
+
+  return (
+    <div className="my-exhibition-wrap">
+      {lists.map((list, index) => {
+        return (
+          <div
+            key={index}
+            className="list"
+            onMouseOver={() => {
+              setHoverIdx(index);
+            }}
+            onMouseLeave={() => {
+              setHoverIdx(-1);
+            }}
+          >
+            <div className={`hover-wrap ${hoverIdx === index && "active"}`}>
+              <ListHoverContent />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MyExhibitionMenu;
+
+const lists = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
