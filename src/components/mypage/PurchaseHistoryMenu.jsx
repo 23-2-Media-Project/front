396370ac5e689f20a4e4eb75b9933846ec28848b
hyeseung <wjoo3@naver.com>
2023-11-26 06:02:33 +0900

    feature: 마이페이지 - 구매내역 날짜 조회 부분 구현

--- src/components/mypage/PurchaseHistoryMenu.jsx
+++ src/components/mypage/PurchaseHistoryMenu.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { ko } from "date-fns/esm/locale";
+
+import { ReactComponent as CalendarSvg } from "../../assets/icons/calendar.svg";
 
 const PurchaseHistoryMenu = () => {
-  return <div>PurchaseHistoryMenu</div>;
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div className="purchase-history-wrap">
+      <div className="date-pickers-wrap">
+        <div className="date-picker-wrap">
+          <DatePicker
+            locale={ko}
+            dateFormat="yyyy-MM-dd"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+          <CalendarSvg style={{ width: "2.5vw" }} />
+        </div>
+        <div className="middle-text">~</div>
+        <div className="date-picker-wrap">
+          <DatePicker
+            locale={ko}
+            dateFormat="yyyy-MM-dd"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+          <CalendarSvg style={{ width: "2.5vw" }} />
+        </div>
+        <div className="search-btn">조회</div>
+      </div>
+    </div>
+  );
 };
 
 export default PurchaseHistoryMenu;
