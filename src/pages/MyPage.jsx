642ef65a747bbb444abff397a229e6bc48f6c675
hyeseung <wjoo3@naver.com>
2023-11-26 00:50:53 +0900

    feature: 사용자 정보 박스 컴포넌트 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -3,10 +3,22 @@ import { Link } from "react-router-dom";
 
 import "../styles/MyPage.scss";
 
+import UserInfoBox from "../components/mypage/UserInfoBox";
+
+/* TODO:
+ * [O] 사용자 정보 박스 구현
+ * [] 메뉴 탭바 구현
+ * [] 나의 전시회 메뉴 구현
+ * [] 좋아요한 전시회 메뉴 구현
+ * [] 방명록 전시회 메뉴 구현
+ * [] 위시리스트 메뉴 구현
+ * [] 구매내역 메뉴 구현
+ */
 const MyPage = () => {
   return (
     <div className="mypage-container">
       {/* FIXME: 임시버튼으로 수정 필요 */}
+      <UserInfoBox />
       <Link to={"/writing"}>
         <button className="writing-btn">전시회 작성</button>
       </Link>
