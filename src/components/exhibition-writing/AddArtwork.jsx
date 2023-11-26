bd0f81ee93a13e42ae194ce3bccd71da99204dcb
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-26 18:42:04 +0900

    html&css: AddArtwork UI 구현 완료

--- src/components/exhibition-writing/AddArtwork.jsx
+++ src/components/exhibition-writing/AddArtwork.jsx
@@ -1,39 +1,47 @@
+import { ReactComponent as CloseIcon } from "../../assets/icons/close.svg";
 import { ReactComponent as HamburgerIcon } from "../../assets/icons/dehaze.svg";
 const AddArtwork = () => {
   return (
     <article className="add-artwork-container">
-      <section>
-        <div className="change-order">
-          <HamburgerIcon />
+      <div className="change-order">
+        <HamburgerIcon />
+      </div>
+      <div className="text-section">
+        <div className="title-section">
+          <span className="title">작품 제목</span>
+          <input
+            type="text"
+            placeholder="작품 제목을 입력하세요."
+            className="input-area"
+          />
         </div>
-        <div className="text-section">
-          <div className="title-section">
-            <span className="title">작품 제목</span>
-            <input type="text" placeholder="작품 제목을 입력하세요." />
-          </div>
-          <div className="add-file-section">
-            <span className="title">
-              작품 첨부
-              <span> *</span>
-            </span>
-            <div className="file-wrapper">
-              <input
-                className="upload-name"
-                placeholder="작품 이미지 또는 영상을 선택하세요."
-                // value={fileName}
-                disabled
-                required
-              />
-              <label htmlFor="file">찾아보기</label>
-              <input type="file" id="file" />
-            </div>
-          </div>
-          <div className="info-section">
-            <span>작품 설명</span>
-            <input type="text" placeholder="작품 설명을 입력하세요." />
+        <div className="add-file-section">
+          <span className="title">
+            작품 첨부
+            <span className="condition"> *</span>
+          </span>
+          <div className="file-wrapper">
+            <input
+              className="upload-name input-area"
+              placeholder="작품 이미지 또는 영상을 선택하세요."
+              // value={fileName}
+              disabled
+              required
+            />
+            <label htmlFor="file">찾아보기</label>
+            <input type="file" id="file" />
           </div>
         </div>
-      </section>
+        <div className="info-section">
+          <span className="title">작품 설명</span>
+          <input
+            type="text"
+            placeholder="작품 설명을 입력하세요."
+            className="input-area"
+          />
+        </div>
+      </div>
+      <CloseIcon className="close-icon" />
     </article>
   );
 };
