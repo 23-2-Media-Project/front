a7e1f23f84e15599a3988c467adf36212690f720
hyeseung <wjoo3@naver.com>
2023-12-01 01:13:45 +0900

    html&css: 전시 상세 페이지 스타일 수정

--- src/pages/ExhibitionDetailPage.jsx
+++ src/pages/ExhibitionDetailPage.jsx
@@ -1,12 +1,40 @@
-import { ReactComponent as FavoriteIC } from "../assets/icons/favorite.svg";
-import imgUrl from "../assets/testImg.png";
+import { useCallback, useState } from "react";
+
 import "../styles/ExhibitionDetailPage.scss";
-// import { ReactComponent as FavoritePurpleIc } from "../assets/icons/favorite-purple.svg";
+
 import { ReactComponent as ShoppingCart } from "../assets/icons/add_shopping_cart.svg";
 import { ReactComponent as ShareIC } from "../assets/icons/share.svg";
 import { ReactComponent as WarningIC } from "../assets/icons/warning.svg";
+import { ReactComponent as FaveActiveSvg } from "../assets/icons/favorite-purple.svg";
+import { ReactComponent as FavoriteIC } from "../assets/icons/favorite.svg";
+import { ReactComponent as CartSvg } from "../assets/icons/cart.svg";
+
+import imgUrl from "../assets/testImg.png";
+
+import AddCartModal from "../components/common/AddCartModal";
 
 const ExhibitionDetailPage = () => {
+  const [clickFavIcon, setClickFavIcon] = useState(false);
+
+  // 좋아요 버튼 클릭 관련
+  const [clickTimes, setClickTimes] = useState(0);
+  const handleClick = useCallback(
+    (e) => {
+      const elmClassList = e.target.classList.value.split(" ");
+      if (!elmClassList.includes("clicked")) {
+        e.target.classList.add("clicked");
+        setClickTimes(clickTimes + 1);
+      } else if (elmClassList.includes("clicked")) {
+        e.target.classList.remove("clicked");
+        setClickTimes(clickTimes - 1);
+      }
+    },
+    [clickTimes]
+  );
+
+  // 장바구니 버튼 클릭 여부
+  const [clickCart, setClickCart] = useState(false);
+
   return (
     <article className="exhibition-detail-container">
       <div className="detail-wrapper">
@@ -16,7 +44,7 @@ const ExhibitionDetailPage = () => {
             <span className="writer">작성자</span>
           </div>
           <div className="default-info-right">
-            <span>작성 날짜</span>
+            <span className="date">2023.09.08</span>
             <span className="keyword">#선택 분야</span>
             <span className="keyword">#선택 주제</span>
           </div>
@@ -55,78 +83,57 @@ const ExhibitionDetailPage = () => {
           <img src={imgUrl} alt="" className="artwork-img" />
         </section>
         <section className="icon-wrapper">
-          <div className="icon-left">
-            <FavoriteIC className="icon" />
-            <span>좋아요 17</span>
+          <div
+            className="icon-left"
+            onClick={() => setClickFavIcon(!clickFavIcon)}
+          >
+            {clickFavIcon ? (
+              <FavoriteIC className="icon" style={{ width: "1.3vw" }} />
+            ) : (
+              <FaveActiveSvg className="icon" style={{ width: "1.3vw" }} />
+            )}
+            <span className="text">
+              {clickFavIcon ? "좋아요 17" : "좋아요 18"}
+            </span>
           </div>
-          <div className="icon-rignt">
-            <ShareIC className="icon" />
-            <WarningIC className="icon" />
+          <div className="icon-right">
+            <ShareIC className="icon" style={{ width: "1.3vw" }} />
+            <WarningIC className="icon" style={{ width: "1.4vw" }} />
           </div>
         </section>
-        <section className="related-goods-list">
-          <div className="related-title">관련 상품 (6)</div>
-          <div className="goods-list">
-            <div className="each-goods">
-              <img src={imgUrl} alt="" />
-              <div className="goods-info-section">
-                <div className="info-left">
-                  <span className="goods-title">상품명</span>
-                  <div className="goods-info">
-                    <span className="price">5,600원</span>
-                    <span className="real-or-not">비실물</span>
+        <section className="related-goods-section">
+          <div className="related-title">
+            관련 상품 ({wishListItems.length})
+          </div>
+          <div className="related-goods-wrap">
+            {wishListItems.map((item, index) => {
+              return (
+                <div key={index} className="item-wrap">
+                  <div className="item-img">
+                    <FaveActiveSvg
+                      onClick={handleClick}
+                      className="favorite-icon"
+                      style={{ width: "1.4vw" }}
+                    />
+                  </div>
+                  <div className="item-content-wrap">
+                    <div className="left-content-wrap">
+                      <div className="name">{item?.name}</div>
+                      <div className="price-real">
+                        <div className="price">{item?.price}</div>
+                        <div className="real">{item?.real}</div>
+                      </div>
+                    </div>
+                    <CartSvg
+                      className="cart-icon"
+                      style={{ width: "1.8vw" }}
+                      onClick={() => setClickCart(true)}
+                    />
                   </div>
                 </div>
-                <div className="add-cart">
-                  <ShoppingCart />
-                </div>
-              </div>
-            </div>
-            <div className="each-goods">
-              <img src={imgUrl} alt="" />
-              <div className="goods-info-section">
-                <div className="info-left">
-                  <span className="goods-title">상품명</span>
-                  <div className="goods-info">
-                    <span className="price">5,600원</span>
-                    <span className="real-or-not">비실물</span>
-                  </div>
-                </div>
-                <div className="add-cart">
-                  <ShoppingCart />
-                </div>
-              </div>
-            </div>
-            <div className="each-goods">
-              <img src={imgUrl} alt="" />
-              <div className="goods-info-section">
-                <div className="info-left">
-                  <span className="goods-title">상품명</span>
-                  <div className="goods-info">
-                    <span className="price">5,600원</span>
-                    <span className="real-or-not">비실물</span>
-                  </div>
-                </div>
-                <div className="add-cart">
-                  <ShoppingCart />
-                </div>
-              </div>
-            </div>
-            <div className="each-goods">
-              <img src={imgUrl} alt="" />
-              <div className="goods-info-section">
-                <div className="info-left">
-                  <span className="goods-title">상품명</span>
-                  <div className="goods-info">
-                    <span className="price">5,600원</span>
-                    <span className="real-or-not">비실물</span>
-                  </div>
-                </div>
-                <div className="add-cart">
-                  <ShoppingCart />
-                </div>
-              </div>
-            </div>
+              );
+            })}
+            {clickCart && <AddCartModal setClickCart={setClickCart} />}
           </div>
         </section>
       </div>
@@ -135,3 +142,31 @@ const ExhibitionDetailPage = () => {
 };
 
 export default ExhibitionDetailPage;
+
+const wishListItems = [
+  {
+    name: "상품1",
+    price: "15,000원",
+    real: "비실물",
+  },
+  {
+    name: "상품2",
+    price: "15,000원",
+    real: "비실물",
+  },
+  {
+    name: "상품3",
+    price: "15,000원",
+    real: "비실물",
+  },
+  {
+    name: "상품4",
+    price: "15,000원",
+    real: "비실물",
+  },
+  {
+    name: "상품5",
+    price: "15,000원",
+    real: "비실물",
+  },
+];
